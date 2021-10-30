7d6b4e0aeaf71538f13a88ef77d9f4e95b1b9a57
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2021-10-30 13:06:16 +0300

    ver. 10

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
             <div class="card w-80 m-3 border-dark">
                 <div class="card-header">
                     <h5 class="card-title">
-                        <a href="/{{$advt->id}}">{{$advt->title}}</a></h5>
+                        <a href="{{route('advts.show', $advt->id)}}">{{$advt->title}}</a></h5>
                 </div>
                 <div class="card-body">
                     <p class="card-text">{{$advt->description}}</p>
@@ -16,14 +16,12 @@
                     <div class="row">
                         <div class="col-auto me-auto fs-7">Author: {{$advt->user->username}}</div>
                         <div class="col-auto">
-{{--                            @if(\Illuminate\Support\Facades\Auth::id() === $advt->user_id)--}}
-                                @can('update', $advt)
-                                    <a class="btn btn-outline-primary btn-sm" href="edit/{{$advt->id}}">Edit</a>
-                                @endcan
-                                @can('delete', $advt)
-                                    <a class="btn btn-outline-primary btn-sm" href="delete/{{$advt->id}}">Delete</a>
-                                @endcan
-{{--                            @endif--}}
+                            @can('update', $advt)
+                                <a class="btn btn-outline-primary btn-sm" href="{{route('advts.create', $advt->id)}}">Edit</a>
+                            @endcan
+                            @can('delete', $advt)
+                                <a class="btn btn-outline-primary btn-sm" href="{{route('advts.delete', $advt->id)}}">Delete</a>
+                            @endcan
                         </div>
                         <div class="col-auto">
                             <small class="text-muted">
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdvtController;
 use App\Http\Controllers\AuthController;
 
 /*
@@ -16,14 +17,22 @@ use App\Http\Controllers\AuthController;
 */
 
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/edit', [HomeController::class, 'form'])->name('advts.create')->middleware('auth');
-Route::post('/edit', [HomeController::class, 'create'])->name('advts.create')->middleware('auth');
+//Route::get('/', [HomeController::class, 'index'])->name('home');
+//Route::get('/edit', [HomeController::class, 'form'])->name('advts.create')->middleware('auth');
+//Route::post('/edit', [HomeController::class, 'create'])->name('advts.create')->middleware('auth');
+//
+//Route::get('/{id}', [HomeController::class, 'advt'])->whereNumber('id');
+//Route::get('/delete/{id}', [HomeController::class, 'delete'])->middleware('auth');
+//Route::get('/edit/{id}', [HomeController::class, 'form'])->whereNumber('id')->middleware('auth');
+//Route::post('/edit/{id}', [HomeController::class, 'edit'])->whereNumber('id')->middleware('auth');
+//
+//Route::post('login', [AuthController::class, 'login'])->middleware('guest');
+//Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
-Route::get('/{id}', [HomeController::class, 'advt'])->whereNumber('id');
-Route::get('/delete/{id}', [HomeController::class, 'delete'])->middleware('auth');
-Route::get('/edit/{id}', [HomeController::class, 'form'])->whereNumber('id')->middleware('auth');
-Route::post('/edit/{id}', [HomeController::class, 'edit'])->whereNumber('id')->middleware('auth');
-
-Route::post('login', [AuthController::class, 'login'])->middleware('guest');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
+Route::get('/', [AdvtController::class, 'index'])->name('home');
+Route::get('/{id}', [AdvtController::class, 'advt'])->whereNumber('id')->name('advts.show');
+Route::post('login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/edit/{id?}', [AdvtController::class, 'form'])->whereNumber('id')->name('advts.create');
+Route::post('/edit/{id?}', [AdvtController::class, 'edit'])->whereNumber('id');
+Route::get('/delete/{id}', [AdvtController::class, 'delete'])->name('advts.delete');
